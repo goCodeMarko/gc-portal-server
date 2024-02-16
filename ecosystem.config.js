148afeed5c6e1814b461912d08c57c647056e568
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-02-16 16:02:39 +0800

    [Enhancement] Improved BE codes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,16 @@
 module.exports = {
   apps: [
     {
+      name: "server",
       script: "server.js",
-      watch: ".",
-    },
-    {
-      script: "./service-worker/",
-      watch: ["./service-worker"],
+      instances: "max",
+      exec_mode: "cluster",
+      env_production: {
+        NODE_ENV: "production",
+      },
+      env_development: {
+        NODE_ENV: "development",
+      },
     },
   ],
-
-  deploy: {
-    production: {
-      user: "SSH_USERNAME",
-      host: "SSH_HOSTMACHINE",
-      ref: "origin/master",
-      repo: "GIT_REPOSITORY",
-      path: "DESTINATION_PATH",
-      "pre-deploy-local": "",
-      "post-deploy":
-        "npm install && pm2 reload ecosystem.config.js --env production",
-      "pre-setup": "",
-    },
-  },
 };
