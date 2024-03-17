797af2b30c448ad0fa9c0421cc6bb0180dbd2418
Patrick Marck Dulaca <39939290+goCodeMarko@users.noreply.github.com>
2024-03-17 12:09:43 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const CLOUDINARY = {
   secret: process.env.CLOUDINARY_API_SECRET,
 };
 
-const DB = "mongodb://localhost:27017/bookstore_local"; //process.env.ATLAS_URI;
+const DB = process.env.ATLAS_URI;
 
 const config = {
   server: SERVER,
