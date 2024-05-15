c412d39ff2b2f6bf8da8839276fc0989f32d67b5
Patrick Marck Dulaca <39939290+goCodeMarko@users.noreply.github.com>
2024-05-15 11:15:36 +0800

    Update pdf.js

--- services/pdf.js
+++ services/pdf.js
@@ -11,12 +11,45 @@ module.exports.generate = async (template, data) => {
   try {
     console.log('--------2.1')
       const browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ],
+        headless: "new",
+      args: [
+        "--disable-features=IsolateOrigins",
+        "--disable-site-isolation-trials",
+        "--autoplay-policy=user-gesture-required",
+        "--disable-background-networking",
+        "--disable-background-timer-throttling",
+        "--disable-backgrounding-occluded-windows",
+        "--disable-breakpad",
+        "--disable-client-side-phishing-detection",
+        "--disable-component-update",
+        "--disable-default-apps",
+        "--disable-dev-shm-usage",
+        "--disable-domain-reliability",
+        "--disable-extensions",
+        "--disable-features=AudioServiceOutOfProcess",
+        "--disable-hang-monitor",
+        "--disable-ipc-flooding-protection",
+        "--disable-notifications",
+        "--disable-offer-store-unmasked-wallet-cards",
+        "--disable-popup-blocking",
+        "--disable-print-preview",
+        "--disable-prompt-on-repost",
+        "--disable-renderer-backgrounding",
+        "--disable-setuid-sandbox",
+        "--disable-speech-api",
+        "--disable-sync",
+        "--hide-scrollbars",
+        "--ignore-gpu-blacklist",
+        "--metrics-recording-only",
+        "--mute-audio",
+        "--no-default-browser-check",
+        "--no-first-run",
+        "--no-pings",
+        "--no-sandbox",
+      ]
       });
       console.log('--------2.2')
+    
     const page = await browser.newPage();
     console.log('--------2.3')
     const html = await compile(template, data);
