61f61f43cfe1e092f6e52bd331a375b7549da975
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-06-24 12:22:43 +0800

    use multer for image upload

--- services/multer.js
+++ services/multer.js
@@ -3,6 +3,7 @@ const multer = require("multer"),
 
 module.exports = multer({
   storage: multer.diskStorage({}),
+  limits: { fileSize: 100 * 1024 * 1024 }, // 50 MB
   fileFilter: (req, file, callback) => {
     callback(null, true);
   },
