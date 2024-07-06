2c3878524739878f7bc35cf761d616f670fde3ad
Patrick Marck D <patrickmarckdulaca@gmail.com>
2024-07-06 09:21:47 +0800

    pwa push notification

--- controllers/serviceWorker.js
+++ controllers/serviceWorker.js
@@ -13,23 +13,21 @@ module.exports.subscribe = async (req, res) => {
 
 module.exports.notify = async (req, res) => {
     try {
-        console.log('-----------------xxxxxxxxx')
-        const subx = {
-            endpoint: 'https://fcm.googleapis.com/fcm/send/duxvw8T2Y10:APA91bHwGkh3l9FRzn5QYglEAbeUfwshdujK_uYBXTE6RZw96BukJrOgTvkjFj9hlFWcSvy3e1Pz5SzQeAl_HZuXXlRt4fZVIlzkkQQaRnQupvLiJBajVYTJjvP8jZVRF8toNkgQYC0',
-            expirationTime: null,
-            keys: {
-                p256dh: 'BIA39EHy4YMbv-MgsEgftxwmy7yWHPMhvzTBUlfN1mif8rHUKc_w-SsLhD_iylXszW1RHN35YoIviAGQmpRHL7M',
-                auth: 'yN3uUArFMBzL5_prlct5Ig'
-            }
-        }
-    
+        console.log('-----------------xxxxxxxxx');
+        const VAPID_PUBLIC_KEY = 'BENFs5s5g4eYPr8DmBtmI7V46TAQhjv22N31JJVVNoicaefJcrM8ezT6XSvt4SUPqk2rt9JfzmuhzTCUr98DPNI';
+        const VAPID_PRIVATE_KEY = 'JTeljBhHFTY9leAHY_M1YwQQY51bvnzRhQHi1MLBoAg';
         const notificationPayload = {
             notification: {
               title: 'New Notification',
               body: 'You have a new message.'
             },
           };
-        
+          
+            webpush.setVapidDetails(
+        'mailto:patrickmarckdulaca@gmail.com',
+        VAPID_PUBLIC_KEY,
+        VAPID_PRIVATE_KEY
+      );
         const x= await webpush.sendNotification({
             endpoint: 'https://fcm.googleapis.com/fcm/send/duxvw8T2Y10:APA91bHwGkh3l9FRzn5QYglEAbeUfwshdujK_uYBXTE6RZw96BukJrOgTvkjFj9hlFWcSvy3e1Pz5SzQeAl_HZuXXlRt4fZVIlzkkQQaRnQupvLiJBajVYTJjvP8jZVRF8toNkgQYC0',
             expirationTime: null,
